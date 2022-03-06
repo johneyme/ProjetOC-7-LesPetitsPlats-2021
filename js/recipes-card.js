b4b665dcbac122d772c7311e2c5ae6de8daf2735
John <john.eyme@gmail.com>
2022-03-06 09:45:51 +0100

    Commencement searchBar

---
+++ js/recipes-card.js
@@ -0,0 +1,46 @@
+function recipesCard() {
+    
+    arrayRecipes.forEach(recipe => {
+     
+     const sectionRecipes = document.querySelector("#affichage-recette")
+     const div = document.createElement("div")
+
+     
+     const ulIngredient = document.createElement("ul")
+     ulIngredient.classList.add('list_ingredient')
+
+     const ingredients = recipe.ingredients
+     ingredients.forEach(ingredient => {
+       const li = document.createElement('li');
+       li.innerHTML = `<span class="ingredient-recette">${ingredient.ingredient}</span>: ${ingredient.quantity || ""} ${ingredient.unit || ''}`
+       ulIngredient.appendChild(li);
+     })
+
+
+     
+     div.setAttribute("class", "carte-recette")
+
+      
+
+      div.innerHTML = `
+      
+      <img class="img-carte" src="/assets/images/pic_200_4263_0_0_100.jpg" />
+      <div class="script-carte">
+        <div class="header-carte">
+          <h2>${recipe.name}</h2>
+          <p class="time-recette"><i class="bi bi-clock"></i> ${recipe.time}Mn</p>
+        </div>
+        <div class="body-carte">
+         ${ulIngredient.outerHTML}
+          <p class="etape-recette">
+            ${recipe.description}
+        </div>
+      </div>`;
+
+      sectionRecipes.appendChild(div)
+     
+
+
+    });
+    
+  }
--- js/script.js
+++ js/script.js
@@ -1,57 +1,24 @@
+let arrayRecipes = []
+
+recipes.forEach(recipe => {
+  arrayRecipes.push(recipe)
+})
+
+console.log(arrayRecipes)
 
 
 
-   
-   
-   function recipesCard() {
-    
-     recipes.forEach(recipe => {
-      
-      const sectionRecipes = document.querySelector("#affichage-recette")
-      const div = document.createElement("div")
+const searchBar = document.getElementById('searchbar')
 
-      
-      const ulIngredient = document.createElement("ul")
-      ulIngredient.classList.add('list_ingredient')
+/*searchBar.addEventListener("change", (e) => {
+  console.log("ok")
+  if (arrayRecipes.includes(searchBar.value) ){
+    console.log("ok2")}
+    }
+  
+  )
+    /*arrayRecipes =[];
+    arrayRecipes.push(recipe)*/
+  
 
-      const ingredients = recipe.ingredients
-      ingredients.forEach(ingredient => {
-        const li = document.createElement('li');
-        li.innerHTML = `<span class="ingredient-recette">${ingredient.ingredient}</span>: ${ingredient.quantity || ""} ${ingredient.unit || ''}`
-        ulIngredient.appendChild(li);
-      })
-
-
-      
-      div.setAttribute("class", "carte-recette")
-
-       
-
-       div.innerHTML = `
-       
-       <img class="img-carte" src="/assets/images/pic_200_4263_0_0_100.jpg" />
-       <div class="script-carte">
-         <div class="header-carte">
-           <h2>${recipe.name}</h2>
-           <p class="time-recette"><i class="bi bi-clock"></i> ${recipe.time}Mn</p>
-         </div>
-         <div class="body-carte">
-          ${ulIngredient.outerHTML}
-           <p class="etape-recette">
-             ${recipe.description}
-         </div>
-       </div>`;
-
-       sectionRecipes.appendChild(div)
-      
-
-       
-
-      
-
-     });
-     
-   }
-
-   recipesCard()
-
+recipesCard()
